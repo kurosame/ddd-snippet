cf3027fc4784a1320574795e141bfe348b41cdff
kurosame <kurosame1227@gmail.com>
2022-01-10 00:06:04 +0900

    fix: components/atoms readme

--- frontend/src/components/atoms/TextInput.tsx
+++ frontend/src/components/atoms/TextInput.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const TextInput: React.VFC = () => {
   const [value, setValue] = useState('')
 
-  return <input type="text" value={value} onChange={() => setValue('a')} />
+  return <input type="text" value={value} onChange={e => setValue(e.target.value)} />
 }
 
 export default TextInput
