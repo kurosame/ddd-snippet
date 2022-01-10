61e61aaaf68de651088d90e405405042e287f749
kurosame <kurosame1227@gmail.com>
2022-01-10 21:13:49 +0900

    add: atoms/Label

---
+++ frontend/src/components/atoms/Label.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Label: React.VFC = () => <span>aa</span>
+
+export default Label
---
+++ frontend/stories/atoms/Label.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from '@/components/atoms/Label'
+
+export default {
+  title: 'atoms'
+}
+
+export const Label = (): JSX.Element => <App />
