dd4142cf1d5c968e28ebed880ba5b25ff610a4bb
kurosame <kurosame1227@gmail.com>
2022-01-11 10:56:54 +0900

    add: molecules/LabeledTextInput

---
+++ frontend_1/src/components/molecules/LabeledTextInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Label from '@/components/atoms/Label'
+import TextInput from '@/components/atoms/TextInput'
+
+const LabeledTextInput: React.VFC = () => (
+  <>
+    <Label />
+    <TextInput />
+  </>
+)
+
+export default LabeledTextInput
---
+++ frontend_1/stories/molecules/LabeledTextInput.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from '@/components/molecules/LabeledTextInput'
+
+export default {
+  title: 'molecules'
+}
+
+export const LabeledTextInput = (): JSX.Element => <App />
