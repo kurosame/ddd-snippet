6a2e487c7cf2d05e284ad06e139a5906faeb30e2
kurosame <kurosame1227@gmail.com>
2022-02-01 15:33:17 +0900

    add: bundle analyzer

--- frontend_1/vite.config.ts
+++ frontend_1/vite.config.ts
@@ -1,12 +1,24 @@
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  build: {
+    rollupOptions: {
+      plugins: [
+        mode === 'analyze' &&
+          visualizer({
+            filename: 'analyzer/index.html',
+            open: true
+          })
+      ]
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src')
     }
   }
-})
+}))
