4fa637e53f4acadb3b68ef61a7446a381a74560f
kurosame <kurosame1227@gmail.com>
2022-02-05 23:12:50 +0900

    add: @storybook/addon-a11y

--- frontend_1/.storybook/main.js
+++ frontend_1/.storybook/main.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
+  addons: ['@storybook/addon-a11y'],
   staticDirs: ['../public'],
   webpackFinal: async config => {
     config.resolve.alias['@'] = path.join(__dirname, '../src')
