6eee6960a2c84bf33d032f902b9597087218f510
kurosame <kurosame1227@gmail.com>
2022-02-06 15:47:05 +0900

    add: react-router

--- frontend_1/src/index.tsx
+++ frontend_1/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { SWRConfig } from 'swr'
+import { Router } from '@/router'
 import { worker } from '@/__mocks__/browser'
-import { Employee } from './components/organisms/Employee'
 
 if (process.env.NODE_ENV === 'development') {
   worker.start({ onUnhandledRequest: 'bypass' })
@@ -15,17 +16,19 @@ class RootComponent extends React.Component {
 
   public render() {
     return (
-      <SWRConfig
-        value={{
-          onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
-            if (error.status === 404) return
-            if (retryCount >= 5) return
-            setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 5000)
-          }
-        }}
-      >
-        <Employee />
-      </SWRConfig>
+      <BrowserRouter>
+        <SWRConfig
+          value={{
+            onErrorRetry: (error, _key, _config, revalidate, { retryCount }) => {
+              if (error.status === 404) return
+              if (retryCount >= 5) return
+              setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 5000)
+            }
+          }}
+        >
+          <Router />
+        </SWRConfig>
+      </BrowserRouter>
     )
   }
 }
---
+++ frontend_1/src/router.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { EmployeePage } from '@/components/pages/Employee'
+
+export const Router: React.VFC = () => (
+  <Routes>
+    <Route path="/" element={<EmployeePage />} />
+    <Route path="employee" element={<EmployeePage />} />
+  </Routes>
+)
